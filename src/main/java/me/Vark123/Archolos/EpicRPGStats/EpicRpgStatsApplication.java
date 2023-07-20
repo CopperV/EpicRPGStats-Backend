17f9aab1cb8f507e106fc364b048d36fe31811b6
CopperV <vark123@interia.pl>
2023-07-20 19:07:12 +0200

    init

---
+++ src/main/java/me/Vark123/Archolos/EpicRPGStats/EpicRpgStatsApplication.java
@@ -0,0 +1,13 @@
+package me.Vark123.Archolos.EpicRPGStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicRpgStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicRpgStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/Vark123/Archolos/EpicRPGStats/EpicRpgStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Vark123.Archolos.EpicRPGStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicRpgStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
